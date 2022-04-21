d961a878f817f54fd2bf63a6f30d54e19c3b4bb6
Kens <afrayin903@gmail.com>
2022-04-21 16:15:51 -0400

    Auto generate primary key value in DB

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,12 +1,19 @@
 package com.cydeo.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
 
     @Id
+    /*Auto generate primary key value in db,
+     no need to generate it by inserting manually using data.sql*/
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fName;
     private String lName;
--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,5 +1,8 @@
-INSERT INTO EMPLOYEES(id,name)
-VALUES(1,'Mike Smith');
+--INSERT INTO EMPLOYEES(id,name)
+--VALUES(1,'Mike Smith');
+
+INSERT INTO student(f_name,l_name,email)
+VALUES('Kahar',' Kasim','Kahar@cydeo.com');
 
 
 --INSERT INTO student(first_name,last_name,email)
