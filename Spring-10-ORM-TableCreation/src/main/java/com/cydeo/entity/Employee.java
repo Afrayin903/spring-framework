aba2407517ff84e1da8e7a78a403123a45b4b13d
Kens <afrayin903@gmail.com>
2022-04-21 10:52:51 -0400

    Updated code

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {  //employee
+
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+}
